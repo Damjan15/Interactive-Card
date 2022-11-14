bac9a8c7c7763d211d65839a5d4c9e80e22e9ac7
Damjan15 <stamenovdamjan20@gmail.com>
2022-11-14 23:11:47 +0100

    fixed wrong styles in config, created the UI for small devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         darkBlue: "#262E38",
-        lightGray: "#262E38",
+        lightGray: "#969FAD",
         mediumGray: "#7C8798",
         orange: "#FC7614",
         veryDarkBlue: "#131518", 
