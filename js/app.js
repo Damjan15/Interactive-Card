db3bac24ba177cd526f1bad71350afbfc532dd88
Damjan15 <stamenovdamjan20@gmail.com>
2022-11-15 11:21:04 +0100

    Rating functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+/*=============== SELECTORS ===============*/
+const ratingList = document.querySelectorAll("#rating-list li");
+const ratingsCard = document.getElementById("ratings");
+const resultsCard = document.getElementById("results");
+const btn = document.getElementById("btn");
+const ratingEl = document.getElementById("rating");
+
+/*=============== FUNCTIONS ===============*/
+for (let i = 0; i < ratingList.length; i++) {
+    
+    ratingList[i].addEventListener("click", () => {
+        let rating = ratingList[i].innerHTML;
+        
+
+        for (let i = 0; i < ratingList.length; i++) {
+            ratingList[i].classList.contains("active");
+            ratingList[i].classList.remove("active");
+        }
+
+        ratingList[i].classList.add("active");
+
+        btn.addEventListener("click", () => {
+            ratingsCard.classList.add("hidden");
+            resultsCard.classList.remove("hidden");
+            resultsCard.classList.add("flex");
+            ratingEl.innerHTML = rating;
+        })
+    })
+    
+}
